e167b8ec28fd9d2fdf749457c9a2d49f1cbf2f57
Jeremy Pinto <jeremy@focus21.io>
2018-06-14 14:42:41 -0400

    Update settings

--- fresh_install.sh
+++ fresh_install.sh
@@ -181,3 +181,5 @@ sudo pip3 install powerline-status
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
+# Install compizconfig settings manager
+sudo apt-get install compizconfig-settings-manager compiz-plugins-extra
