245efc74b6de4fd2cb93ee264a36463522531db3
Jeremy Pinto <jerpint@gmail.com>
2019-03-11 17:47:31 -0400

    Fix typo

---
+++ basic_setup.sh
@@ -0,0 +1,24 @@
+# Add zsh, vundle, and set up dotfiles
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/jerpint/fresh_install.git
+
+source ~/fresh_install/dotfiles/createsymlinks.sh
+
+# Add bash_aliases
+echo "if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi" >> ~/.bashrc
+
+echo "# Use ZSH" >> ~/.bashrc
+echo "export SHELL=/bin/zsh" >> ~/.bashrc
+echo "exec /bin/zsh -l" >> ~/.bashrc
+
+
+# Install vundle
+cd && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install tpmgit clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+tmux source ~/.tmux.conf
